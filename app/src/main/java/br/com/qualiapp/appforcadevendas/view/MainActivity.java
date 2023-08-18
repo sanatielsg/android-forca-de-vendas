1235e127a7fe51f6c81e7ef79b6876aae511911e
Dev Qualiapp <dev.qualiapp@gmail.com>
2023-08-18 12:16:54 -0300

    Initial commit

---
+++ app/src/main/java/br/com/qualiapp/appforcadevendas/view/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.qualiapp.appforcadevendas.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.qualiapp.appforcadevendas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
