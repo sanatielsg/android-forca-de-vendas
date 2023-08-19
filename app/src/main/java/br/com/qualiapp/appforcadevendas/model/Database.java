e64606e6338051f858d3866a192fc9572b11e3af
Dev Qualiapp <dev.qualiapp@gmail.com>
2023-08-19 07:38:15 -0300

    tabela CLIENTE

--- app/src/main/java/br/com/qualiapp/appforcadevendas/model/Database.java
+++ app/src/main/java/br/com/qualiapp/appforcadevendas/model/Database.java
@@ -16,6 +16,22 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE CLIENTE (" +
+                "CLI_CODIGO         INTEGER," +
+                "CLI_NOME           VARCHAR(50), " +
+                "CLI_FANTASIA       VARCHAR(50)," +
+                "CLI_ENDERECO       VARCHAR(50)," +
+                "CLI_BAIRRO         VARCHAR(50)," +
+                "CLI_CEP            VARCHAR(50)," +
+                "CID_CODIGO         INTEGER," +
+                "CLI_CONTATO        VARCHAR(20)," +
+                "CLI_NASCIMENTO     VARCHAR(13)," +
+                "CLI_CPFCNPJ        VARCHAR(25)," +
+                "CLI_RGINSCRICAOEST VARCHAR(40)," +
+                "CLI_EMAIL          VARCHAR(50)," +
+                "CLI_ENVIADO        CHAR(1)," +
+                "CLI_CHAVE          VARCHAR(100)" +
+                ")");
 
     }
 
